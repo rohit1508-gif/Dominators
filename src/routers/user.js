526ca553c00db6b3d87389f1730cd3d815116119
ROHIT AGRAWAL <64243388+rohit1508-gif@users.noreply.github.com>
2020-11-10 08:12:16 +0530

    Init Commit

--- src/routers/user.js
+++ src/routers/user.js
@@ -3,6 +3,16 @@ const router = new express.Router()
 const User = require('../models/user')
 const auth = require('../middleware/auth')
 const { TokenExpiredError } = require('jsonwebtoken')
+router.post('/users',async(req,res)=>{
+    const user = new User(req.body)
+    try{
+    await user.save()
+    const token = await user.gettoken()
+    res.status(201).send({user,token})}
+    catch(error){res.status(400).send("error" + error)
+    console.log(error)
+}
+})
 router.post('/users/login',async(req,res)=>{try{
     const user = await User.findByCridential(req.body.name,req.body.password)
     const token = await user.gettoken()
